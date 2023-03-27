47cd9585c32e5b98ce8c31a28c75323880f2a4e6
Michael Mueller <michael.mueller@uos.de>
2023-03-27 16:30:11 +0200

    Explicit types for config.h

--- src/mx/memory/config.h
+++ src/mx/memory/config.h
@@ -8,16 +8,16 @@ public:
     /**
      * @return Number of maximal provided NUMA regions.
      */
-    static constexpr auto max_numa_nodes() { return 2U; }
+    static constexpr std::uint8_t max_numa_nodes() { return 8U; }
 
     /**
      * @return Interval of each epoch, if memory reclamation is used.
      */
-    static constexpr auto epoch_interval() { return std::chrono::milliseconds(50U); }
+    static constexpr std::chrono::milliseconds epoch_interval() { return std::chrono::milliseconds(50U); }
 
     /**
      * @return True, if garbage is removed local.
      */
-    static constexpr auto local_garbage_collection() { return false; }
+    static constexpr bool local_garbage_collection() { return false; }
 };
 } // namespace mx::memory
