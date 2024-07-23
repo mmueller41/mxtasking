5aa7a905f7c053a12d4e7d73867a4094ac687be2
Michael Mueller <michael.mueller@uos.de>
2024-07-23 16:24:54 +0200

    Adapted maximum number of cores to evaluation platform.

--- src/mx/tasking/config.h
+++ src/mx/tasking/config.h
@@ -12,7 +12,7 @@ public:
     };
 
     // Maximal number of supported cores.
-    static constexpr auto max_cores() { return 128U; }
+    static constexpr auto max_cores() { return 64U; }
 
     // Maximal size for a single task, will be used for task allocation.
     static constexpr auto task_size() { return 64U; }
