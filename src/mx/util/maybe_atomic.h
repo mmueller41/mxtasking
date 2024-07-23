7b8735c721f921a3d796b945c9acad114619ccfd
Michael Mueller <michael.mueller@uos.de>
2024-07-23 16:27:49 +0200

    Fix to make mxtasking compilable with gcc.

--- src/mx/util/maybe_atomic.h
+++ src/mx/util/maybe_atomic.h
@@ -6,8 +6,8 @@ namespace mx::util {
 /**
  * The maybe_atomic<T> makes T atomic in ARM hardware but none-atomic on x86_64.
  */
-#if defined(__x86_64__) && (!defined(__has_feature) || !__has_feature(thread_sanitizer))
-template <typename T> using maybe_atomic = T;
+#if defined(__x86_64__) && (!defined(__has_feature)) //|| !__has_feature(thread_sanitizer))
+template <typename T> using maybe_atomic = std::atomic<T>;
 #else
 template <typename T> using maybe_atomic = std::atomic<T>;
 #endif
